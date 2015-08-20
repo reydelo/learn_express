109890841fad47feea43301da55160764493ee1c
reydelo <reyna.deloge@gmail.com>
2015-08-20 10:30:08 -0600

    add additional routing

--- app.js
+++ app.js
@@ -2,13 +2,23 @@
 var express = require('express'),
   app = express();
 
+  var vegetables = [
+          "Carrots",
+          "Cucumber",
+          "Peas"
+           ];
 // a "GET" request to "/" will run the function below
 app.get("/", function (req, res) {
   // send back the response: 'Hello World'
   res.send("Hello World");
 });
 
+app.get("/vegetables", function (req, res) {
+  // send all the veggies
+  res.send(vegetables.join(", "));
+});
+
 // start the server
 app.listen(3000, function () {
-  console.log("Starting a server on localhost:3000");
+  console.log("Go to localhost:3000");
 });
